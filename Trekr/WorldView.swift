c0fee2339f33778104375c028b519640cdaf10a2
Mayur Vaity <mayur.m.vaity@icloud.com>
2024-08-15 00:40:16 +0530

    Implement MapAnnotations to pin locations on map and NavigationLink to click on MapAnnotations.

--- Trekr/WorldView.swift
+++ Trekr/WorldView.swift
@@ -21,8 +21,24 @@ struct WorldView: View {
     
     var body: some View {
         NavigationStack {
-            //map vw, centered on abv region location
-            Map(coordinateRegion: $region)
+            //coordinateRegion - map vw, centered on abv region location
+            //annotationItems - also adding all the coordinates from locations array on the map with a pin of country flag
+            Map(coordinateRegion: $region, annotationItems: locations.places) {
+                location in
+                //to create pins on map of shape of these countries flags
+                MapAnnotation(coordinate: CLLocationCoordinate2D(
+                    latitude: location.latitude,
+                    longitude: location.longitude)) {
+                        //NavigationLink - to open DetailView vw with this location, by tapping on below Image vw
+                        NavigationLink(destination: DetailView(location: location)) {
+                            Image(location.country)
+                                .resizable()
+                                .clipShape(.rect(cornerRadius: 10))
+                                .frame(width: 80, height: 40)
+                                .shadow(radius: 3)
+                        }
+                    }
+            }
                 .navigationTitle("Locations")
         }
     }
